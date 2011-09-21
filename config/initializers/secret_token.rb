d04bf4771d07d4be5abeb396d960783a744400b2
Tephanie Flora Cabading <tephanie.cabading@awsol-i.com>
2011-09-21 11:06:47 +0800

    awstest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = 'db4fd3884203436f5a02890f8839446c0faad5bc0991dbfae6ebc623a25cc5253a5196b84efd6682b3ea0a48cdfba8da838d03bb0897b6053dac33ad3a7898ec'
